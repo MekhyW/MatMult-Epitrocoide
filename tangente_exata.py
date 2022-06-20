67cd0053dc52f5440d49d2f1180af74b599e6e0f
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-06-19 23:53:35 -0300

    Update tangente_exata.py

--- tangente_exata.py
+++ tangente_exata.py
@@ -8,9 +8,9 @@ t2 = [0, 4*pi, 4*pi*(0 + (202/4155)), 4*pi*(0 - (202/4155)), 4*pi*(1 + (202/4155
 4*pi*(0 + (456/3677)), 4*pi*(0 - (456/3677)), 4*pi*(1 + (456/3677)), 4*pi*(1 - (456/3677))]
 
 for valor in t1:
-    ponto = [(-6 * sin(valor)) + (9 * sin(4.5*valor)), 0]
+    ponto = [(6 * cos(valor)) - (2 * cos(4.5*valor)), 0]
     print("Ponto de tangencia horizontal: {}".format(ponto))
 
 for valor in t2:
-    ponto = [0, (6 * cos(valor)) - (9 * cos(4.5*valor))]
+    ponto = [0, (6 * sin(valor)) - (2 * sin(4.5*valor))]
     print("Ponto de tangencia vertical: {}".format(ponto))
