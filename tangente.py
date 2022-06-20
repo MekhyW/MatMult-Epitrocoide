8de5c5f0e1e6135f22623169e9402f5a27518f4a
Rafael Katri <rkatri20033@gmail.com>
2022-06-19 21:58:58 -0300

    correcao

--- tangente.py
+++ tangente.py
@@ -1,23 +1,23 @@
 import numpy as np
 from cmath import cos, sin
 
-# Passo a ser utilizado para iterar os valores de x(t)
+# Passo a ser utilizado para iterar os valores de gama(t)
 dt = 0.001
 
 t_verticais = []
 t_horizontais = []
 
 for t in np.arange(0,4*np.pi,dt):
-    # Armazenando o valor de x e y a cada instante
-    x = 6 * cos(t) - 2 * cos(4.5 * t)
-    y = 6 * sin(t) - 2 * sin(4.5 * t)
+    # Armazenando o valor de x' e y' a cada instante
+    x_linha = -6 * np.sin(t) + 9 * np.sin(4.5 * t)
+    y_linha = 6 * np.cos(t) - 9 * np.cos(4.5 * t)
 
     # Se valor de X for aproximadamente 0, o ponto é vertical
-    if abs(x) < 0.005:
+    if abs(x_linha) < 0.005:
         t_verticais.append(t)
 
     # Se o valor de Y for aproximadamente 0, o ponto é horizontal
-    if abs(y) < 0.005:
+    if abs(y_linha) < 0.005:
         t_horizontais.append(t)
 
 
