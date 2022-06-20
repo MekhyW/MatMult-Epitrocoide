de5f380cd00eaf43fa7a27f48cad63897ee4bc9c
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-06-20 00:31:08 -0300

    Update tangente_exata.py

--- tangente_exata.py
+++ tangente_exata.py
@@ -4,13 +4,14 @@ t1 = [4*pi*(0 + (39/2573)), 4*pi*(0 - (39/2573)), 4*pi*(1 + (39/2573)), 4*pi*(1 
 4*pi*(0 + (137/1546)), 4*pi*(0 - (137/1546)), 4*pi*(1 + (137/1546)), 4*pi*(1 - (137/1546)), 
 4*pi*(0 - (555/3928)), 4*pi*(1 - (555/3928))]
 
-t2 = [0, 4*pi, 4*pi*(0 + (202/4155)), 4*pi*(0 - (202/4155)), 4*pi*(1 + (202/4155)), 4*pi*(1 - (202/4155)), 
-4*pi*(0 + (456/3677)), 4*pi*(0 - (456/3677)), 4*pi*(1 + (456/3677)), 4*pi*(1 - (456/3677))]
+t2 = [0, 4*pi, 4*pi*(0 + (202/4155)), 4*pi*(0 - (202/4155)), 4*pi*(1 + (202/4155)), 
+4*pi*(1 - (202/4155)), 4*pi*(0 + (456/3677)), 4*pi*(0 - (456/3677)), 4*pi*(1 + (456/3677)), 
+4*pi*(1 - (456/3677))]
 
 for valor in t1:
-    ponto = [(6 * cos(valor)) - (2 * cos(4.5*valor)), 0]
+    ponto = [(6 * cos(valor)) - (2 * cos(4.5*valor)), (6 * sin(valor)) - (2 * sin(4.5*valor))]
     print("Ponto de tangencia horizontal: {}".format(ponto))
 
 for valor in t2:
-    ponto = [0, (6 * sin(valor)) - (2 * sin(4.5*valor))]
+    ponto = [(6 * cos(valor)) - (2 * cos(4.5*valor)), (6 * sin(valor)) - (2 * sin(4.5*valor))]
     print("Ponto de tangencia vertical: {}".format(ponto))
